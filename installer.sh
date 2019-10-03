064cec1b9ff7257a9681e97e79143eadab426a73
Victor Morales <victor.morales@intel.com>
2019-10-03 01:38:51 -0700

    Add info entries in the provisioning process

--- installer.sh
+++ installer.sh
@@ -13,9 +13,13 @@ set -o xtrace
 set -o errexit
 set -o nounset
 
+echo "Install Kubernetes"
 KRD_ACTIONS=("install_k8s" "install_helm")
 curl -fsSL https://raw.githubusercontent.com/electrocucaracha/krd/master/aio.sh | KRD_ACTIONS_DECLARE=$(declare -p KRD_ACTIONS) bash
 
+echo "Install infrastructure (Grafana and InfluxDB)"
 helm install stable/influxdb --name metrics-db -f influxdb_values.yml
 helm install stable/grafana --name metrics-dashboard -f grafana_values.yml
+
+echo "Create Kubernetes resources"
 kubectl apply -f k6.yml
